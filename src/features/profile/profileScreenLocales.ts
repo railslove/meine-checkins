8adfd55c7d5d6aab1311b12287b1972470eeb5e0
Javier Carrillo Milla <stringparser@gmail.com>
2021-03-31 11:37:31 +0200

    feature #7: define locales per screen

---
+++ src/features/profile/profileScreenLocales.ts
@@ -0,0 +1,11 @@
+export default {
+  title: 'Persönliche Daten',
+  subtitle: 'Mit deinen Daten kannst du automatisch bei allen Anbietern einchecken.',
+  address: 'Addresse',
+  lastName: 'Nachname',
+  firstName: 'Vorname',
+  phoneNumber: 'Telefonnummer',
+  dataPrivacyInfo:
+    'Deine Daten werden sicher lokal auf deinem Gerät gespeichert und nur zum Checkin an den jeweiligen Anbieter übertragen.',
+  submit: 'speichern',
+};
---
+++ src/features/start/startScreenLocales.ts
@@ -0,0 +1,5 @@
+export default {
+  title: 'Meine Check-ins',
+  description: 'Einmalig Kontaktdaten eingeben und überall einchecken',
+  submit: "los geht's",
+};
