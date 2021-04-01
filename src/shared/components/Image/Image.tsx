caed3a3203b7bdb6cccfc16eefd9820abc179ae0
Javier Carrillo Milla <stringparser@gmail.com>
2021-04-01 09:51:12 +0200

    feature, #6: layout fixes for images

--- src/shared/components/Image/Image.tsx
+++ src/shared/components/Image/Image.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
 
 import {StyleSheet, Image as RNImage, ImageProps as RNImageProps} from 'react-native';
+import Box from 'src/shared/components/Layout/Box';
 
 const style = StyleSheet.create({
   root: {
-    maxWidth: '100%',
-    maxHeight: '50%',
+    maxWidth: '90%',
   },
 });
 
 export type ImageProps = RNImageProps;
 
 const Image: React.FC<ImageProps> = props => (
-  <RNImage style={style.root} resizeMode="center" {...props} />
+  <Box maxHeight="50%" display="flex" alignItems="center" justifyContent="center">
+    <RNImage style={style.root} resizeMode="contain" {...props} />
+  </Box>
 );
 
 export default Image;
