d141eee1db6fa9f35b621048595b3081724bfa6a
Javier Carrillo Milla <stringparser@gmail.com>
2021-04-09 20:02:26 +0200

    adds missing library after uuid

--- src/__mocks__/setupMocks.js
+++ src/__mocks__/setupMocks.js
@@ -4,6 +4,10 @@
 import mockPermissions from 'react-native-permissions/mock';
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock';
 
+jest.mock('react-native-get-random-values', () => ({
+  getRandomBase64: jest.fn(),
+}));
+
 jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage);
