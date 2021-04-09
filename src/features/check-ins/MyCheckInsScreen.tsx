fe0d6e9b2bfbe0a574ee641c68db7dfd2456cd01
Javier Carrillo Milla <stringparser@gmail.com>
2021-04-09 18:56:40 +0200

    adds open link to imprint

--- src/features/check-ins/MyCheckInsScreen.tsx
+++ src/features/check-ins/MyCheckInsScreen.tsx
@@ -2,6 +2,7 @@ import React, {useCallback} from 'react';
 import {useSelector} from 'react-redux';
 import {useTranslation} from 'react-i18next';
 
+import OpenLinkService from 'src/shared/services/OpenLinkService';
 import {CheckInsRoutes} from 'src/features/check-ins/constants';
 import {useAppNavigation} from 'src/shared/hooks/navigationHooks';
 
@@ -11,24 +12,24 @@ import Paragraph from 'src/shared/components/Typography/Paragraph';
 import ButtonLink from 'src/shared/components/Button/ButtonLink';
 import TopLevelView from 'src/shared/components/Layout/TopLevelView';
 
+import Box from 'src/shared/components/Layout/Box';
 import {ScanRoutes} from 'src/features/scan/ScanStackNavigator';
 import CheckInsList from 'src/features/check-ins/components/CheckInsList';
 import EmptyCheckInsList from 'src/features/check-ins/components/EmptyCheckInsList';
-import Box from 'src/shared/components/Layout/Box';
 import {LAYOUT_PADDING_HORIZONTAL} from 'src/shared/components/Layout/constants';
 
 const MyCheckInsScreen: React.FC = () => {
   const {t} = useTranslation('myCheckInsScreen');
   const navigate = useAppNavigation();
 
-  const {current, items} = useSelector(state =>  state.checkIns);
+  const {current, items} = useSelector(state => state.checkIns);
 
   const handleNavigateFAQ = useCallback(() => {
     navigate.navigate(CheckInsRoutes.FAQ);
   }, [navigate]);
 
   const handleNavigateToImprint = useCallback(() => {
-    navigate.navigate(CheckInsRoutes.Imprint);
+    OpenLinkService.openImprint();
   }, [navigate]);
 
   const handleNavigateToCurrent = useCallback(() => {
