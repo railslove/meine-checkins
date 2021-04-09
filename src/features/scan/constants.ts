10bfb7d5dbfa25e7e807f59c78a0a2b77d62a124
Javier Carrillo Milla <stringparser@gmail.com>
2021-04-09 12:25:50 +0200

    remove duplicated TEST_PROVIDER

--- src/features/scan/constants.ts
+++ src/features/scan/constants.ts
@@ -5,13 +5,6 @@ export const PROVIDER_SITE_MESSAGE = {
   checkOutFailure: 'CheckOutFailure',
 };
 
-export const TEST_PROVIDER = {
-  id: Math.random().toString(32).slice(2),
-  // url: 'https://check-in-provider.vercel.app',
-  url:
-    'https://rcvr.app/checkin?a=8c2bb0ea-5a6c-4219-8a6d-428f245d7aa7&k=LeDTVuNGteBiKoGmZH9jN%2FmQNuphhcg%2FksroGKnAGmw%3D',
-};
-
 export enum ScanRoutes {
   key = 'ScanRoutesKey',
   MyCheckIns = 'MyCheckIns',
