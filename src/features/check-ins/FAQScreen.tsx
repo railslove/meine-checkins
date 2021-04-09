41367414ee11fbec45b285b5b5f12d72901eac9d
Javier Carrillo Milla <stringparser@gmail.com>
2021-04-09 19:59:25 +0200

    adds FAQ

--- src/features/check-ins/FAQScreen.tsx
+++ src/features/check-ins/FAQScreen.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
-import TopLevelView from 'src/shared/components/Layout/TopLevelView';
+import React, {useState} from 'react';
+import {useTranslation} from 'react-i18next';
+
+import Space from 'src/shared/components/Layout/Space';
 import Title from 'src/shared/components/Typography/Title';
+import Accordion from 'src/shared/components/Accordion/Accordion';
+import TopLevelView from 'src/shared/components/Layout/TopLevelView';
 
 const FAQScreen: React.FC = () => {
+  const {t} = useTranslation('faqScreen');
+  const [openAccordion, setOpenAccordion] = useState(-1);
+
+  const handleOpenAccordion = (index: number) => () => {
+    setOpenAccordion(index === openAccordion ? -1 : index);
+  };
+
+  const items = t('items', {returnObjects: true});
+
   return (
     <TopLevelView>
-      <Title>FAQScreen</Title>
+      <Space.V s={10} />
+      <Title split={false}>{t('title')}</Title>
+      <Space.V s={20} />
+
+      {items.map((el, index) => {
+        const {title, content} = el;
+
+        return (
+          <Accordion
+            key={index}
+            open={index === openAccordion}
+            title={title}
+            content={content || 'empty'}
+            onOpen={handleOpenAccordion(index)}
+          />
+        );
+      })}
     </TopLevelView>
   );
 };
---
+++ src/shared/components/Accordion/Accordion.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import {StyleSheet, Text, View} from 'react-native';
+
+import {toDpFromPixel} from 'src/shared/theme/util';
+
+import Space from 'src/shared/components/Layout/Space';
+import PlusIcon from 'src/shared/components/Icon/PlusIcon';
+import Paragraph from 'src/shared/components/Typography/Paragraph';
+import Description from 'src/shared/components/Typography/Description';
+
+export type AccordionProps = {
+  open?: boolean;
+  title: string;
+  content: React.ReactNode;
+  onOpen: () => void;
+};
+
+const styles = StyleSheet.create({
+  root: {
+    paddingVertical: toDpFromPixel(12),
+
+    display: 'flex',
+    flexDirection: 'column',
+
+    borderTopWidth: toDpFromPixel(2),
+    borderTopColor: '#E5ECFC',
+  },
+  center: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  headerTextContainer: {
+    flex: 1,
+    marginRight: toDpFromPixel(30),
+  },
+  headerText: {
+    fontSize: toDpFromPixel(15),
+    fontWeight: '700',
+    fontFamily: 'Inter-Bold',
+    lineHeight: toDpFromPixel(22),
+  },
+});
+
+const Accordion: React.FC<AccordionProps> = ({open, title, content, onOpen}) => {
+  return (
+    <View style={styles.root}>
+      <TouchableOpacity style={styles.header} onPress={onOpen} activeOpacity={1}>
+        <View style={styles.headerTextContainer}>
+          <Text style={styles.headerText}>{title}</Text>
+        </View>
+        <View style={styles.center}>
+          <PlusIcon isSelected={!open} />
+        </View>
+      </TouchableOpacity>
+      {open ? (
+        <>
+          <Space.V s={10} />
+          <Paragraph>{content}</Paragraph>
+        </>
+      ) : null}
+    </View>
+  );
+};
+
+export default Accordion;
