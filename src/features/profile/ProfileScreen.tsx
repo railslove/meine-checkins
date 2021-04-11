4e6fa1fd4054e396871a086f169383a009498174
Javier Carrillo Milla <stringparser@gmail.com>
2021-04-11 11:37:19 +0200

    adds input validation

--- src/features/profile/ProfileScreen.tsx
+++ src/features/profile/ProfileScreen.tsx
@@ -1,61 +1,78 @@
-import {useDispatch, useSelector} from 'react-redux';
+import React from 'react';
+import {useTheme} from 'react-native-paper';
 import {useTranslation} from 'react-i18next';
-import React, {useCallback, useState} from 'react';
+import {Controller, useForm} from 'react-hook-form';
+import {useDispatch, useSelector} from 'react-redux';
 
 import User from 'src/shared/models/User';
+import {ScanRoutes} from 'src/features/scan/constants';
+import {toDpFromPixel} from 'src/shared/theme/util';
 
 import Box from 'src/shared/components/Layout/Box';
 import Space from 'src/shared/components/Layout/Space';
 import Button from 'src/shared/components/Button/Button';
 import Title from 'src/shared/components/Typography/Title';
-import TextInput from 'src/shared/components/Form/TextInput';
-import TopLevelView from 'src/shared/components/Layout/TopLevelView';
-import {ScanRoutes} from 'src/features/scan/ScanStackNavigator';
 import LockIcon from 'src/shared/components/Icon/LockIcon';
 import Subtitle from 'src/shared/components/Typography/Subtitle';
-import {saveUserThunk} from 'src/shared/redux/effects/userThunks';
 import Paragraph from 'src/shared/components/Typography/Paragraph';
+import TopLevelView from 'src/shared/components/Layout/TopLevelView';
+import {saveUserThunk} from 'src/shared/redux/effects/userThunks';
 import {useAppNavigation} from 'src/shared/hooks/navigationHooks';
+import TextInput, {TextInputProps} from 'src/shared/components/Form/TextInput';
 
 const ProfileScreen: React.FC = () => {
   const {t} = useTranslation('profileScreen');
   const user = useSelector(state => state.user.item);
+  const theme = useTheme();
   const dispatch = useDispatch();
   const navigation = useAppNavigation();
 
-  const [address, setAddress] = useState(user?.address || '');
-  const [lastName, setLastName] = useState(user?.lastName || '');
-  const [firstName, setFirstName] = useState(user?.firstName || '');
-  const [phoneNumber, setPhoneNumber] = useState(user?.phoneNumber || '');
+  const {
+    control,
+    formState: {errors, submitCount, isValid},
+    getValues,
+    handleSubmit,
+  } = useForm<User>({
+    defaultValues: user,
+  });
 
-  const handleAddressChange = useCallback((value: string) => {
-    setAddress(value);
-  }, []);
-
-  const handleLastNameChange = useCallback((value: string) => {
-    setLastName(value);
-  }, []);
-
-  const handleFirstNameChange = useCallback((value: string) => {
-    setFirstName(value);
-  }, []);
-
-  const handlePhoneNumberChange = useCallback((value: string) => {
-    setPhoneNumber(value);
-  }, []);
-
-  const handleSubmit = () => {
-    const user: User = {
-      address,
-      lastName,
-      firstName,
-      phoneNumber,
-    };
+  const handleSave = handleSubmit(user => {
+    console.log('submit', user);
 
     dispatch(saveUserThunk({user})).then(() => {
       navigation.navigate(ScanRoutes.ScanQRCode);
     });
+  });
+
+  const renderTextInput = (name: keyof User, inputProps: TextInputProps = {}) => {
+    return (
+      <Controller
+        name={name}
+        rules={{required: true}}
+        control={control}
+        render={({field: {onChange, onBlur}}) => {
+          const value = getValues(name);
+          const label = t(name);
+          const hasError = errors[name] != null;
+
+          return (
+            <TextInput
+              dataDetectorTypes="all"
+              {...inputProps}
+              label={hasError ? label : undefined}
+              value={value}
+              error={hasError}
+              onBlur={onBlur}
+              placeholder={hasError ? undefined : label}
+              onChangeText={value => onChange(value)}
+            />
+          );
+        }}
+      />
+    );
   };
+
+  const canSubmit = isValid;
 
   return (
     <TopLevelView>
@@ -69,35 +86,59 @@ const ProfileScreen: React.FC = () => {
       <Space.V s={10} />
 
       <Box flex={1} flexDirection="column">
-        <TextInput
-          value={firstName}
-          placeholder={t('firstName')}
-          autoCompleteType="name"
-          onChangeText={handleFirstNameChange}
-        />
-        <TextInput
-          value={lastName}
-          placeholder={t('lastName')}
-          autoCompleteType="name"
-          onChangeText={handleLastNameChange}
-        />
-        <TextInput
-          placeholder={t('address')}
-          value={address}
-          autoCompleteType="street-address"
-          onChangeText={handleAddressChange}
-        />
-        <TextInput
-          value={phoneNumber}
-          placeholder={t('phoneNumber')}
-          keyboardType="phone-pad"
-          autoCompleteType="tel"
-          onChangeText={handlePhoneNumberChange}
-        />
+        <Box display="flex" flexDirection="row">
+          <Box flex={1}>
+            {renderTextInput('firstName', {
+              autoCompleteType: 'name',
+            })}
+          </Box>
+          <Box flex={1} marginLeft={toDpFromPixel(5)}>
+            {renderTextInput('lastName', {
+              autoCompleteType: 'name',
+            })}
+          </Box>
+        </Box>
 
-        <Box display="flex" flexDirection="row" alignItems="center" flexWrap="wrap">
-          <Box width="82%" display="flex" flexDirection="row" alignItems="center">
-            <Box borderRadius={7} marginRight={20}>
+        {renderTextInput('streetAddress', {
+          autoCompleteType: 'street-address',
+          dataDetectorTypes: 'address',
+        })}
+
+        <Box display="flex" flexDirection="row">
+          <Box flex={1}>
+            {renderTextInput('postalCode', {
+              keyboardType: 'number-pad',
+              autoCompleteType: 'postal-code',
+              dataDetectorTypes: 'phoneNumber',
+            })}
+          </Box>
+          <Box flex={1} marginLeft={toDpFromPixel(5)}>
+            {renderTextInput('city', {
+              autoCompleteType: 'street-address',
+              dataDetectorTypes: 'address',
+            })}
+          </Box>
+        </Box>
+
+        {renderTextInput('phoneNumber', {
+          keyboardType: 'phone-pad',
+          autoCompleteType: 'tel',
+        })}
+
+        <Space.V s={2.5} />
+        <Box
+          display="flex"
+          alignItems="center"
+          flexDirection="column"
+          justifyContent="center"
+          marginHorizontal="7.5%"
+        >
+          <Box display="flex" flexDirection="row" alignItems="flex-start">
+            <Box
+              borderRadius={theme.roundness}
+              marginRight={toDpFromPixel(20)}
+              marginTop={toDpFromPixel(5)}
+            >
               <LockIcon />
             </Box>
             <Box>
@@ -105,12 +146,10 @@ const ProfileScreen: React.FC = () => {
             </Box>
           </Box>
 
-          <Box>
-            <Space.V s={15} />
-            <Button fullWidth onPress={handleSubmit}>
-              {t('submit')}
-            </Button>
-          </Box>
+          <Space.V s={10} />
+          <Button fullWidth onPress={handleSave} disabled={!canSubmit}>
+            {t('submit')}
+          </Button>
         </Box>
       </Box>
     </TopLevelView>
