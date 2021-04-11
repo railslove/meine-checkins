2b06113004a737b6d9816e7592cdc134e0ff7e24
Javier Carrillo Milla <stringparser@gmail.com>
2021-04-11 18:26:51 +0200

    remove unused import

--- src/shared/components/Layout/TopLevelView.tsx
+++ src/shared/components/Layout/TopLevelView.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View, ViewStyle} from 'react-native';
+import {StyleSheet, ViewStyle} from 'react-native';
 
 import {ScrollView} from 'react-native-gesture-handler';
 import {SafeAreaView} from 'react-native-safe-area-context';
