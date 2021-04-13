8c32ffd71e55d398a1e902c01490d8f41a48ec66
Javier Carrillo Milla <stringparser@gmail.com>
2021-04-13 17:38:14 +0200

    remove unused async

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -4,7 +4,7 @@ import App from 'src/main';
 import {render} from '@testing-library/react-native';
 
 describe('App', () => {
-  it('renders without crashing', async () => {
+  it('renders without crashing', () => {
     const result = render(<App />);
 
     expect(result.container).toBeDefined();
