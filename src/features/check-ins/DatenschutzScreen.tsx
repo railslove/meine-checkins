f332c53df8b5ca9c66f1049490e4cb515214f6bc
Javier Carrillo Milla <stringparser@gmail.com>
2021-05-11 19:02:11 +0200

    cleanup

--- src/features/check-ins/DatenschutzScreen.tsx
+++ src/features/check-ins/DatenschutzScreen.tsx
@@ -1,19 +1,9 @@
 import React from 'react';
-import {StyleSheet, TextInput} from 'react-native';
 import {useTranslation} from 'react-i18next';
-
-import {px2dp} from 'src/shared/styles/createStyles';
 
 import Title from 'src/shared/components/Typography/Title';
 import TopLevelView from 'src/shared/components/Layout/TopLevelView';
 import MultilineText from 'src/shared/components/Typography/MultilineText';
-
-const styles = StyleSheet.create({
-  text: {
-    fontSize: px2dp(12),
-    fontFamily: 'Inter-Regular',
-  },
-});
 
 const DatenschutzScreen = () => {
   const {t} = useTranslation('datenschutz');
--- src/features/check-ins/ImprintScreen.tsx
+++ src/features/check-ins/ImprintScreen.tsx
@@ -1,34 +1,12 @@
 import React from 'react';
 import {useTranslation} from 'react-i18next';
-import {StyleSheet, TextInput} from 'react-native';
 
-import {px2dp} from 'src/shared/styles/createStyles';
 import {COLOGNE_CITY_LOGO_MARK} from 'src/features/check-ins/imprintLocales';
 
 import Image from 'src/shared/components/Image/Image';
 import Title from 'src/shared/components/Typography/Title';
 import TopLevelView from 'src/shared/components/Layout/TopLevelView';
 import MultilineText from 'src/shared/components/Typography/MultilineText';
-
-const style = StyleSheet.create({
-  text: {
-    fontSize: px2dp(12),
-    fontFamily: 'Inter-Regular',
-  },
-});
-
-const renderTextContent = (content: string) => {
-  return (
-    <TextInput
-      dataDetectorTypes={['link', 'phoneNumber']}
-      editable={false}
-      multiline={true}
-      value={content}
-      style={style.text}
-      scrollEnabled={false}
-    />
-  );
-};
 
 const ImprintScreen = () => {
   const {t} = useTranslation('imprint');
